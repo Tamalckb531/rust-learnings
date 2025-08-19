4684f5582ca7f91fb6f1e852675d07854b2e4a7e
Tamal <ckbtamaldipnew@gmail.com>
2025-08-19 19:27:39 +0600

    Variables -> numbers

--- kirat-rust-1/src/Code/variables/numbers.rs
+++ kirat-rust-1/src/Code/variables/numbers.rs
@@ -1,10 +1,7 @@
 fn main() {
-    //? Numbers :
     let x: u32 = 846;
     let y: i8 = 56;
     let z: i32 = -142;
     let h: f64 = -142.56;
     println!("x: {}, y: {}, z: {}. h: {}", x, y, z, h);
-
-    //? Boolean:
 }
