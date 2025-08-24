faf0e7ad5550b2fe910e8e7f511e21b21a91ea9f
Tamal <ckbtamaldipnew@gmail.com>
2025-08-24 20:52:29 +0600

    Ownerships -> add mdx note

--- kirat-rust-1/src/main.rs
+++ kirat-rust-1/src/main.rs
@@ -3,4 +3,10 @@ fn main() {
     println!("{} ", s1);
     let s2: String = s1;
     println!("{} ", s2);
+
+    let a = 23;
+    println!("{} ", a);
+    let b = a;
+    println!("{} ", b);
+    println!("{} ", a);
 }
